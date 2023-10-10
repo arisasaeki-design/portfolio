489297c26d031ed3fde616b6c47ce5169fd5f1b9
arisasaeki-design <146195756+arisasaeki-design@users.noreply.github.com>
2023-10-10 19:20:35 +0900

    Add files via upload

--- js/toggle.js
+++ js/toggle.js
@@ -9,10 +9,12 @@ $(function() {
 
 $(function() {
     $('.header-navigation-link').on('click', function () {
-      $('.header-navigation-wrap').removeClass('header-navigation-wrap-open');      
+      $('.header-navigation-wrap').removeClass('header-navigation-wrap-open');  
+      $('.header-line-top').removeClass('header-line-top-open');      
+      $('.header-line-middle').removeClass('header-line-middle-open');     
+      $('.header-line-bottom').removeClass('header-line-bottom-open');      
     })
   })
-
 
 
 
